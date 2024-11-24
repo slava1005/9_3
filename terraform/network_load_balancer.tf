202ef64132ebc9e77c8541682422a7dcafb6e622
VJACHESLAV <114395964+slava1005@users.noreply.github.com>
2024-11-24 18:51:54 +0300

    Create network_load_balancer.tf

---
+++ terraform/network_load_balancer.tf
@@ -0,0 +1,25 @@
+resource "yandex_lb_network_load_balancer" "network-balancer" {
+  name = "lamp-balancer"
+  deletion_protection = "false"
+  listener {
+    name = "http-check"
+    port = 80
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+  attached_target_group {
+    target_group_id = yandex_compute_instance_group.group-vms.load_balancer[0].target_group_id
+    healthcheck {
+      name = "http"
+      interval = 2
+      timeout = 1
+      unhealthy_threshold = 2
+      healthy_threshold = 5
+      http_options {
+        port = 80
+        path = "/"
+      }
+    }
+  }
+}
